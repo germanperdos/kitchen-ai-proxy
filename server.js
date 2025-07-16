9b1881dd27f514e50fe108a1fa8f9654467f4314
maloy1337 <maloy@example.com>
2025-07-16 10:48:44 -0100

    обновил версию модели Replicate

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.post('/generate', async (req, res) => {
       "Authorization": `Token ${replicateToken}`
     },
     body: JSON.stringify({
-      version: "fdb0199e807dbe1b8b61a7188e4f9ea26e850dbff2a083fdb4530a1e6c54baf6",
+      version: "76604baddc85b1b4616e1c6475eca080da339c8875bd4996705440484a6eac38",
       input: {
         prompt: "high quality kitchen interior design, " + userPrompt,
         width: 768,
